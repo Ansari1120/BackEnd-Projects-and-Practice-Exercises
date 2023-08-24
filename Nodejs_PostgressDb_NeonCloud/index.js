b709421dc83674f3f567f443b973365c1e26fa95
Ahmed Ali Ansari <ahmedaliansari506@gmail.com>
2023-08-24 23:28:36 +0500

    "NeonCloud_PostgresSqlDb_Nodejs_expressJs"

---
+++ Nodejs_PostgressDb_NeonCloud/index.js
@@ -0,0 +1,15 @@
+const postgres = require("postgres");
+require("dotenv").config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+
+const sql = postgres(URL, { ssl: "require" });
+
+async function getPgVersion() {
+  const result =
+    await sql`SELECT * FROM employees`;
+  console.log(result);
+}
+
+getPgVersion();
